7eb9e7a66ff69167ce9e859bcc596196aee66350
Sterling Collins <scstercollins@gmail.com>
2017-04-28 17:13:16 -0500

    Update HX711_test.c

--- HX711_test.c
+++ HX711_test.c
@@ -1,37 +1,45 @@
 #include <msp430g2553.h>
-#include <hx711.h>
+#include "hx711.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-#define ARRAYSIZE 200
+#define ARRAYSIZE 100
 
 void main(void) {
 
-
-  // stop watchdog timer
 	WDTCTL = WDTPW | WDTHOLD;
-  // set up bit 0 of P1 as output
-	//P1DIR = 0x01;
-	//P2DIR = 0x00;
-  // intialize bit 0 of P1 to 0
-	//P1OUT = 0x00;
-	unsigned long reading;
-	signed int cbReading = 0;
+	unsigned long reading = 0;
+	unsigned int cReading = 0;
+	unsigned int tReading = 0;
+	unsigned int temp = 0;
 	unsigned int myArray[ARRAYSIZE] = { 0 };
+	unsigned int myArrayR[ARRAYSIZE] = { 0 };
 	unsigned int counter = 0;
 	unsigned int zeroed = 0;
-	reading = ReadCount();
+
+
+	reading = ReadCount() / 1;
 	zeroed = Tare(reading);
+	temp = (reading/1000);
+	//__delay_cycles(2000000);// must wait to read
+	//cReading = (reading / 1000) - temp;
+
+
+
 	while (1) // infinite loop
 	{
+		__delay_cycles(125000);//8 readings
 		reading = ReadCount(); // calls function to return scale readings then assigns to variable reading
-		cbReading = (reading / 1000) - 8229; // Calibration equation (10000 8124 is off 8232 is on)
-		myArray[counter] = cbReading; // places calibrated readings into array for safekeeping
+		cReading = (reading / 1000) - 8100; // Calibration equation (add a failsafe?)
+		tReading = (reading / 1000) - zeroed;
+		myArray[counter] = cReading; // places calibrated readings into array for safekeeping
+		myArrayR[counter] = tReading;
 		counter++; //increments counter
-		__delay_cycles(125000); // 8 readings per second
-		if(counter > 200) // resets counter to 0 when past 200
+		if(counter > 100) // resets counter to 0 when past 200
 		{
 			counter = 0;
 		}
 	}
+
+
 }
