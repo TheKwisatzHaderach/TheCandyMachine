e6afb50f1bc465c5fdc0a79ca12a008e38a51f18
Sterling Collins <scstercollins@gmail.com>
2017-05-04 15:45:49 -0500

    Update Individual servos.c

--- Individual servos.c
+++ Individual servos.c
@@ -1,16 +1,20 @@
 #include "msp430g2553.h"
 #include "Servos.h"
 
+#define A 1
+#define B 0
+
 void main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;
 	while(1)
 	{
+		// calls servo function from hearder file, to run one servo comment others out
 		ServoONE();
 		ServoTWO();
 		ServoTHREE();
-		ServoFOUR();
-
+		ServoFOUR(A);
+		ServoFOUR(B);
 	}
 
 }
